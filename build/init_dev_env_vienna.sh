fef7358ea93f7d1f81c1badcc96bf94c4f9bdd81
Andreas Manessinger <andreas.manessinger@wien.gv.at>
2021-05-02 11:47:17 +0200

    Use fake apache_proxy.pem as proxy.pem on CI server Log in test script for debugging build error

--- build/init_dev_env_vienna.sh
+++ build/init_dev_env_vienna.sh
@@ -5,6 +5,12 @@ cd "$(dirname "$(dirname "$0")")"
 bash ./api/bin/init_dev_env_vienna.sh
 bash ./demo/bin/init_dev_env_vienna.sh
 
+if [ ! -z "${DSC_RUNNING_ON_CI_SERVER}" ] ; then
+  # We have no cert in repo, but orion-proxy needs one to start up
+  # Use fake. Removed at end of ci_test/test.sh
+  cp ./build/apache_proxy.pem ./api/proxy.env
+fi
+
 rm -f docker-compose.yml
 ln -s docker-compose-vienna.yml docker-compose.yml
 
--- ci_test/test.sh
+++ ci_test/test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -z "${DSC_RUNNING_ON_CI_SERVER}" ] ; then
+  set -x
+fi
+
 . api/process.env
 
 EXIT_CODE=0
@@ -18,7 +22,14 @@ done
 if [[ ${EXIT_CODE} -eq 0 ]] ; then
     echo "### SUCCESS"
 else
+    docker-compose logs
     echo "### FAILURE"
+fi
+
+if [ ! -z "${DSC_RUNNING_ON_CI_SERVER}" ] ; then
+  # We have no cert in repo, but orion-proxy needs one to start up
+  # Created by ./build/init_dev_env_vienna.sh
+  rm -f ./api/proxy.env
 fi
 
 exit "${EXIT_CODE}"
